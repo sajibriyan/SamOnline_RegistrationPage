4d3c0a467d32c1859516e0fbd0130eb97da70ab4
Sajib Riyan <sajib.miah.riseuplabs@gmail.com>
2022-06-17 12:32:38 +0600

    Demo_14-06-22

--- src/test/java/LoginAutomation.java
+++ src/test/java/LoginAutomation.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginAutomation {
@@ -9,9 +8,9 @@ public class LoginAutomation {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://sam.rultest2.com/account/");
-        driver.findElement(By.xpath("driver.get(\"https://sam.rultest2.com/account/\");")).sendKeys("rakibtorzo");
+        driver.findElement(By.xpath("//*[@id=\"stm-lms-login\"]/span/form/div/span[1]/div/input")).sendKeys("rakibtorzo");
         driver.findElement(By.xpath("(//input[contains(@name,'password')])[1]")).sendKeys("12345678");
-        WebElement login = driver.findElement(By.name("commit"));
-        login.click();
+        driver.findElement(By.xpath("//*[@id=\"stm-lms-login\"]/span/form/div/div/a")).click();
     }
 }
+
